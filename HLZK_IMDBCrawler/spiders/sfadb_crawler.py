4957341c065a1434400a17f81b225cf8318b7892
HeavenDuke <trashlhc@163.com>
2019-04-12 13:32:21 +0800

    完成奖项数据爬虫的基本逻辑

--- HLZK_IMDBCrawler/spiders/sfadb_crawler.py
+++ HLZK_IMDBCrawler/spiders/sfadb_crawler.py
@@ -15,14 +15,14 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-class BookSpider(scrapy.Spider):
+class SfadbSpider(scrapy.Spider):
     name = "sfadb"
     allowed_domains = ["www.sfadb.com"]
 
     def __init__(self, *args, **kwargs):
-        super(BookSpider, self).__init__(*args, **kwargs)
+        super(SfadbSpider, self).__init__(*args, **kwargs)
         self.base_url = "http://www.sfadb.com"
-        self.start_urls = ['']
+        self.start_urls = ['http://www.sfadb.com/Hugo_Awards_2019']
         self.interval = 3
         option = webdriver.ChromeOptions()
         option.add_argument("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/59.0.3071.109 Chrome/59.0.3071.109 Safari/537.36")
@@ -32,24 +32,34 @@ class BookSpider(scrapy.Spider):
     def strip(self, string):
         return string.replace(" ", "").replace("\t", "").replace("\n", "")
 
-    def parse_year(self, response):
+    def parse(self, response):
+
         years_link = response.css("#menunav a::attr(href)").extract()
-        years = response.css("#menunav a::text").extract()
-        result = []
-        for i in range(len(years)):
-            result.append({
-                "year": self.strip(years[i]),
+        _years = response.css("#menunav a::text").extract()
+        years = []
+        for i in range(len(_years)):
+            years.append({
+                "year": self.strip(_years[i]),
                 "link": self.base_url + "/" + years_link[i]
             })
-        return result
-
-    def parse(self, response):
-        years = self.parse_year(response)
         for item in years:
             self.award_list[item["year"]] = {}
             yield Request(item["link"], callback = self.parse_year)
+            break
 
     def parse_year(self, response):
+        year_data = self.award_list[response.url[-4:]]
+        categories = response.css(".categoryblock")
+        for category in categories:
+            if len(category.css(".category::text").extract()) != 0:
+                cname = self.strip(category.css(".category::text").extract()[0])
+            else:
+                cname = self.strip(category.css(".category > span::text").extract()[0])
+            if cname not in year_data:
+                year_data[cname] = []
+            item_containers = category.css('ul>li')
+            for item_container in item_containers:
+                year_data[cname].append("".join(item_container.css("::text").extract()))
         pass
 
     def closed(self, reason):
@@ -61,4 +71,14 @@ class BookSpider(scrapy.Spider):
         self.browser.quit()
 
     def save_result(self):
+        category_data = self.year2category()
         pass
+
+    def year2category(self):
+        category_data = {}
+        for year in self.award_list:
+            for category in self.award_list[year]:
+                if category not in category_data:
+                    category_data[category] = {}
+                category_data[category][year] = self.award_list[year][category]
+        return category_data
